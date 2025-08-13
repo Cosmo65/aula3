af28627248411271788137ddebd1c3e8c12150fe
Cosmo65 <leololopes@gmail.com>
2025-08-13 20:40:29 -0300

    comitando codigo do ovo frito

---
+++ src/funcoes.py
@@ -0,0 +1,10 @@
+passos = [
+    "Pegar 1 ovo",
+    "Aquecer a frigideira",
+    "Adicionar um fio de óleo",
+    "Quebrar o ovo na frigideira",
+    "Cozinha por alguns minutos",
+    "Servir"
+]
+for i, passo in enumerate(passos, start=1):
+    print(f"{i}, {passo}")
